1e1c23aa7ebc4407aa07b7988c6e5dfb144290fb
Getu Gebre <getugebre@Getus-MacBook-Air.local>
2019-02-19 19:22:42 -0600

    complete arthimetics

--- CalculatorNew/ViewController.swift
+++ CalculatorNew/ViewController.swift
@@ -21,40 +21,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelOutlet: UILabel!
     @IBOutlet var operatorOutlet: [UIButton]!
     
-    @IBAction func pointPressed(_ sender: UIButton) {
-    }
+   
     
     @IBAction func percentPressed(_ sender: UIButton) {
     }
     @IBAction func signPressed(_ sender: UIButton) {
+        
+//        number = "-" + number
+//        if !storedArray.isEmpty{
+//            storedArray[0] = "-\(storedArray[0])"
+//            labelOutlet.text = storedArray[0]
+//        }
+        
     }
     @IBAction func clearPressed(_ sender: UIButton) {
+        
+        labelOutlet.text = "0"
     }
     @IBAction func operatorPressed(_ sender: UIButton) {
         sender.backgroundColor = UIColor.yellow
         mathSign = sender.title(for: .normal)!
-        
-        if !number.isEmpty {
-            
-        
-        if storedArray.count <= 2 {
-            pushNumber(input: number)
-            
-        } else {
-            pushNumber(input: mathSign)
-            
-        }
-        
-            
-          
-        }
-        
-        pushNumber(input: mathSign)
-        
-       
-        
-
-        number = ""
+        calculation()
         
     }
     
@@ -66,17 +53,26 @@ class ViewController: UIViewController {
         }
         number += sender.title(for: .normal)!
         
-        
-        
         labelOutlet.text = number
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        labelOutlet.text = "0"
     }
-    var newCalculator : Calculator!
-    //newCalculator = Calculator()
+    
+    func calculation (){
+        if !number.isEmpty {
+            if storedArray.count <= 2 {
+                pushNumber(input: number)
+            } else {
+                pushNumber(input: mathSign)
+            }
+        }
+        pushNumber(input: mathSign)
+        number = ""
+    }
     
     func stringToInt (input: String) -> Double {
         return Double(input)!
@@ -99,23 +95,22 @@ class ViewController: UIViewController {
         
                 if storedArray.count < 3 {
                     if storedArray.count == 2 && operators.contains(input){
-                        //print(storedArray)
+                        
                         storedArray[1] = input
-                        //print(storedArray)
+                        
                     } else {
                         storedArray.append(input)
-                        //print(storedArray)
                     }
                     
                 } else {
                     outP = mathematicalOperation(num1: stringToInt(input: storedArray[0]), mathSign: storedArray[1], num2: stringToInt(input: storedArray[2]))
                     labelOutlet.text = outP
-                    //print(storedArray)
                     storedArray = [outP, input]
-                    //print(storedArray)
+                    
         }
        
     }
+    
     
 }
 
